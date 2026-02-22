a45bc347f00191ad3a6ccf681031634740da8907
Aaron Askew <aaronaskew@gmail.com>
2026-02-22 09:58:07 -0600

    added cricket sounds

---
+++ CREDITS.md
@@ -0,0 +1,3 @@
+# Credits
+
+- Crickets At Night - Raw sound by Defelozedd94 -- https://freesound.org/s/522299/ -- License: Creative Commons 0
--- scripts/bug.gd
+++ scripts/bug.gd
@@ -4,4 +4,3 @@ extends Sprite2D
 
 func _process(_delta: float):
 	self.position = lerp(self.position, get_viewport().get_mouse_position(), 0.1)
-
